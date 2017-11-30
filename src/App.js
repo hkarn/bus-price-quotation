75564050a1841a7b6b631a5da4e74d1965ca8324
Håkan Kindström Arnoldson <hakan@arnoldson.net>
2017-11-30 17:28:09 +0100

    address completion works

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './scss/css/App.css';
+import Autocomplete from 'react-google-autocomplete';
 
 class App extends Component {
+  
+  state = {loaded: false}
+
+  onPlaceSelected() {
+    console.log("selected")
+  }
+
+  componentDidMount() {
+    const script = document.createElement("script");
+    script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBn5jU3NE8VcLZPbOSV-BuG3a6Ainz6LQA&libraries=places";
+    script.async = true
+    script.onload = () => {
+      this.setState({loaded: true})
+        
+      }
+      
+    
+    document.body.appendChild(script);
+  }
+  
+  
+  
+  
   render() {
+
+    let auto = null;
+    if (this.state.loaded) {
+      auto = <Autocomplete
+        style={{width: '90%'}}
+        onPlaceSelected={(place) => {
+        console.log(place);}}
+      />;
+    } else {
+      auto = "Hello Not loaded yet";
+    }
+
+
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +50,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {auto}
+    
+      
       </div>
     );
   }
